a76d10c2f164d5634de33a959581e32fb17228d6
Peter Kassenaar <info@kassenaar.com>
2019-11-12 10:24:24 +0100

    Added example 630-nested-routes

---
+++ examples/630-nested-routes/src/components/MainNavigation/MainNavigation.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {NavLink} from "react-router-dom";
+import styles from './MainNavigation.module.css'
+
+class MainNavigation extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink exact activeClassName={styles.activeRoute} to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact activeClassName={styles.activeRoute} to="/about">About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact activeClassName={styles.activeRoute} to="/contact">Contact</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        )
+            ;
+    }
+}
+
+export default MainNavigation;
