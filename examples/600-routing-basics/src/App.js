3d56cfd5a0077d8a8975b3b588ad609e0f79cb0d
Peter Kassenaar <info@kassenaar.com>
2022-02-10 17:09:11 +0100

    updated routing example

--- examples/600-routing-basics/src/App.js
+++ examples/600-routing-basics/src/App.js
@@ -7,6 +7,8 @@ import Routes from "./components/Routes/Routes";
 
 function App() {
     return (
+        // You can (and will!) also find <BrowserRouter> in other places,
+        // like in app.js or index.js, or other components in the app.
         <BrowserRouter>
             <div className="container">
                 <Header/>
