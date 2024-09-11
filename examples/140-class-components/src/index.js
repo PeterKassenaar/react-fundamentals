5889d8fec0ef3927f73cfb04d156b48574d3617e
PeterKassenaar <info@kassenaar.com>
2024-09-11 14:16:47 +0200

    Updated example code

--- examples/140-class-components/src/index.js
+++ examples/140-class-components/src/index.js
@@ -5,7 +5,11 @@ import App from "./components/App";
 // bootstrap stuff
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(
-	<App/>,
-	document.getElementById('root')
+// Using <App /> as the Top Level Component.
+// This component is now constructed as a class { ... }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
