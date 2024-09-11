3357edd647540b9a76b61b9263d157299cb53bda
PeterKassenaar <info@kassenaar.com>
2024-09-11 15:49:22 +0200

    Updated example code

--- examples/230-conditional-rendering/src/components/CountryDetail/CountryDetail.js
+++ examples/230-conditional-rendering/src/components/CountryDetail/CountryDetail.js
@@ -1,44 +1,41 @@
 // CountryDetail.js - show details of a specific country
-import React, {Component} from 'react'
+import React from 'react'
 
 // A pure presentational component
-class CountryDetail extends Component {
+const CountryDetail = ({country}) => {
 
-    render() {
-        const country = this.props.country;
-        return (
-            <div>
-                <h2>{country.name}</h2>
-                <ul className="list-group">
+    return (
+        <div>
+            <h2>{country.name}</h2>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    id: {country.id}
+                </li>
+                <li className="list-group-item">
+                    name: {country.name}
+                </li>
+                <li className="list-group-item">
+                    capital: {country.capital}
+                </li>
+                <li className="list-group-item">
+                    details: {country.details}
+                </li>
+                <li className="list-group-item">
+                    <img
+                        className="img-fluid"
+                        src={require(`../../img/countries/${country.img}`)} alt={country.name}/>
+                </li>
+                {/*Adding the badge list item conditionally (Like a Vue Computed Property). */}
+                {country.cost > 4000 ?
                     <li className="list-group-item">
-                        id: {country.id}
+                        <span className="badge bg-warning">Expensive!</span>
                     </li>
-                    <li className="list-group-item">
-                        name: {country.name}
-                    </li>
-                    <li className="list-group-item">
-                        capital: {country.capital}
-                    </li>
-                    <li className="list-group-item">
-                        details: {country.details}
-                    </li>
-                    <li className="list-group-item">
-                        <img
-                            className="img-fluid"
-                            src={require(`../../img/countries/${country.img}`)} alt={country.name}/>
-                    </li>
-                    {/*Adding the badge list item conditionally (Like a Vue Computed Property). */}
-                    {country.cost > 4000 ?
-                        <li className="list-group-item">
-                            <span className="badge badge-warning">Expensive!</span>
-                        </li>
-                        : ''
-                    }
-                    {/*Workshop: add a badge if a destination is 'on sale' (i.e. cheaper than 1000)*/}
-                </ul>
-            </div>
-        )
-    }
+                    : ''
+                }
+                {/*Workshop: add a badge if a destination is 'on sale' (i.e. cheaper than 1000)*/}
+            </ul>
+        </div>
+    )
 }
 
 export default CountryDetail
--- examples/230-conditional-rendering/src/components/VacationPicker/VacationPicker.js
+++ examples/230-conditional-rendering/src/components/VacationPicker/VacationPicker.js
@@ -1,29 +1,26 @@
 // VacationPicker.js - a simple React Vacation Picker component.
 // We opted for using class based components here.
-import React, {Component} from 'react';
+import React from 'react';
 
-class VacationPicker extends Component {
+const VacationPicker = ({countries, select}) => {
 
-    render() {
-        return (
-            <div>
-                <ul className="list-group">
-                    {this.props.countries.map(country =>
-                        <li className="list-group-item"
-                            key={country.id}
-                            id={country.id}
-                            title={country.details}
-                            onClick={() => this.props.select(country)}
-                        >
-                            {country.name}
-                            {/*Workshop: add a badge directly in this list if a
+    return (
+        <div>
+            <ul className="list-group">
+                {countries.map(country =>
+                    <li className="list-group-item"
+                        key={country.id}
+                        id={country.id}
+                        title={country.details}
+                        onClick={() => select(country)}>
+                        {country.name}
+                        {/*Workshop: add a badge directly in this list if a
                             destination is 'on sale' or 'expensive'. */}
-                        </li>
-                    )}
-                </ul>
-            </div>
-        );
-    }
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default VacationPicker
