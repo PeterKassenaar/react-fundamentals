a831d102877e68fe93214584e975f9573bbdd5ae
PeterKassenaar <info@kassenaar.com>
2025-03-03 20:23:08 +0100

    feat: update example 410-multiple-textboxes

---
+++ examples/410-multiple-textboxes/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from "./components/App";
+
+// bootstrap stuff
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// The Top Level Component
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
