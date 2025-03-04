b6bb212f99f9be2471571c684e5f80ded1fce803
PeterKassenaar <info@kassenaar.com>
2025-03-04 20:17:27 +0100

    feat: update example 800-redux-intro

--- examples/800-redux-intro/src/constants/action-types.js
+++
@@ -1,6 +0,0 @@
-// 3. Add Action Types as constants to
-// avoid typos in the reducers and actions
-
-export const INCREMENT = "INCREMENT";
-export const DECREMENT = "DECREMENT";
-export const RESET = "RESET";
