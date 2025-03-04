1f12246dc6a6061638484209c5b0c522b16b1a3b
PeterKassenaar <info@kassenaar.com>
2025-03-04 19:22:17 +0100

    feat: update example 600-routing-basics

--- examples/600-routing-basics/src/main.jsx
+++ examples/600-routing-basics/src/main.jsx
@@ -1,7 +1,13 @@
+// main.jsx - the starting file of the application
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './components/App.jsx'
+
+// routing stuff
 import {BrowserRouter} from "react-router-dom";
+
+// import bootstrap stuff
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
