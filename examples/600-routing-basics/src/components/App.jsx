3e0463455f9dc89804aa8cb858fa8292010107d8
PeterKassenaar <info@kassenaar.com>
2026-04-23 09:26:04 +0200

    chore: minor updates to routing example

--- examples/600-routing-basics/src/components/App.jsx
+++ examples/600-routing-basics/src/components/App.jsx
@@ -1,7 +1,7 @@
 import logo from '../img/logo-react-small.png';
 
 // import routing stuff
-import {Routes, Route, Navigate, BrowserRouter} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import MainNavigation from "./MainNavigation/MainNavigation.jsx";
 import {lazy, Suspense} from "react";
 
@@ -18,30 +18,32 @@ import Contact from "../routes/Contact/Contact.jsx";
 // components to be shown inside the router
 const App = () => {
     return (
-        <BrowserRouter>
-            <div className="container">
-                <h1>
-                    <img src={logo} alt="React logo" width={80}/>
-                    React routing app</h1>
-                <hr/>
-                <MainNavigation/>
-                {/* Wrap Routes inside Suspense for lazy-loaded components */}
-                {/*<Suspense fallback={<div>Loading...</div>}>*/}
-                <Routes>
-                    <Route path='/home' element={ <Home/>}/>
-                    <Route path='/about' element={<About />}/>
-                    <Route path='/contact' element={<Contact/>}/>
-                    {/*Navigate to home if no matching route is found*/}
-                    <Route
-                        path="*"
-                        element={<Navigate to="/home" replace/>}
-                    />
-                </Routes>
-                {/*</Suspense>*/}
-                <hr/>
+        <>
+            {/* BrowserRouter is already provided in src/main.jsx. */}
+            {/* Keeping only one Router in the app prevents nested Router errors. */}
+        <div className="container">
+            <h1>
+                <img src={logo} alt="React logo" width={80}/>
+                React routing app</h1>
+            <hr/>
+            <MainNavigation/>
+            {/* Wrap Routes inside Suspense for lazy-loaded components */}
+            {/*<Suspense fallback={<div>Loading...</div>}>*/}
+            <Routes>
+                <Route path='/home' element={ <Home/>}/>
+                <Route path='/about' element={<About />}/>
+                <Route path='/contact' element={<Contact/>}/>
+                {/*Navigate to home if no matching route is found*/}
+                <Route
+                    path="*"
+                    element={<Navigate to="/home" replace/>}
+                />
+            </Routes>
+            {/*</Suspense>*/}
+            <hr/>
 
-            </div>
-        </BrowserRouter>
+        </div>
+        </>
     );
 }
 
